7b4a510da118ed008c072c56f1191657ddb76c6a
Pranjal <pranjal.kabra.mat23@itbhu.ac.in>
2026-03-11 18:05:56 +0530

    Add BankAccount model

--- src/com/bank/Main.java
+++ src/com/bank/Main.java
@@ -1,4 +1,4 @@
-package com.bank.models;
+package com.bank;
 
 public class Main {
     public static void main(String[] args){
---
+++ src/com/bank/models/BankAccount.java
@@ -0,0 +1,22 @@
+package com.bank.models;
+
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance){
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    public String getAccountNumber(){
+        return this.accountNumber;
+    }
+    public String getAccountHolderName(){
+        return this.accountHolderName;
+    }
+    public double getBalance(){
+        return this.balance;
+    }
+}
